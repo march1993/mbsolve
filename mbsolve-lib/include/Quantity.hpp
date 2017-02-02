1c972fb53fb02038e7393e385f8423a4ca7091a8
Michael Riesch <michael.riesch@tum.de>
2017-02-02 13:38:00 +0100

    Cont'd Win support 2.

--- mbsolve-lib/include/Quantity.hpp
+++ mbsolve-lib/include/Quantity.hpp
@@ -1,8 +1,6 @@
 #ifndef QUANTITY_H
 #define QUANTITY_H
 
-#define _USE_MATH_DEFINES
-#include <cmath>
 #include <string>
 #include <mbsolve-lib_EXPORTS.hpp>
 #include <Types.hpp>
@@ -47,10 +45,11 @@ public:
 
 };
 
-static MBSOLVE_LIB_EXPORT Quantity HBAR = 1.05457266e-34;
-static MBSOLVE_LIB_EXPORT Quantity MU0 =  M_PI * 4e-7;
-static MBSOLVE_LIB_EXPORT Quantity EPS0 = 8.854187817e-12;
-static MBSOLVE_LIB_EXPORT Quantity E0 =  1.60217733e-19;
+const Quantity PI = 3.14159265359;
+const Quantity HBAR = 1.05457266e-34;
+const Quantity MU0 =  PI() * 4e-7;
+const Quantity EPS0 = 8.854187817e-12;
+const Quantity E0 =  1.60217733e-19;
 
 }
 
